dd1cc9b2ac027e3c9eebb006e31c4bfe8e696eac
scienceanna <hughesannae@gmail.com>
2025-08-08 12:12:24 +0100

    a few edits for revisions

--- 1AB_analysis_lego.R
+++ 1AB_analysis_lego.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(brms)
 library(tidybayes)
 library(patchwork)
-options(mc.cores = 8)
+#options(mc.cores = 8)
 
 theme_set(theme_bw())
 
@@ -172,6 +172,98 @@ bs %>% select(`conjunction - colour` = "d_colour", `conjunction - shape` = "d_sh
   ggtitle("(iii) Differences Between Conditions") -> plt_blocks3
 
 #rm(m, bs, lego_search_summary)
+
+# Old data - with slightly incorrect tray set up
+# However, basic results remain the same
+
+lego_search <- read_csv('data/lego_search_old.csv') %>%
+  pivot_longer(c(colour, shape, conjunction), names_to = "condition", values_to = "rt") %>%
+  rename(participant = "Participant") %>%
+  mutate(logrt = log(rt),
+         condition = as_factor(condition),
+         condition = fct_relevel(condition, "conjunction"))
+
+lego_search_summary <- lego_search %>%
+  group_by(condition) %>%
+  summarise(mean_RT = mean(rt),
+            sd_RT = sd(rt))
+
+ggplot(lego_search_summary, aes(condition, mean_RT)) + 
+  geom_bar(stat = "identity") + 
+  geom_errorbar(aes(ymin = mean_RT - sd_RT, ymax = mean_RT + sd_RT), position = "dodge", width = 0.25) + 
+  theme_bw()
+
+ggplot(lego_search, aes(log(rt), fill = condition)) + geom_histogram(alpha = 0.3)
+
+# define priors
+
+my_priors <- c(prior(normal(1.5, 1), class = "b"),
+               prior(exponential(1), class = "sigma"))
+
+validate_prior(rt ~ 0 + condition, 
+               data = lego_search,
+               prior = my_priors,
+               family = lognormal())
+
+m <- brm(logrt ~ 0 + condition, 
+         data = lego_search,
+         prior = my_priors,
+         sample_prior = "only")
+
+lego_search %>% modelr::data_grid(condition) %>% 
+  add_epred_draws(m) %>%
+  select(-.chain, -.iteration) %>%
+  ggplot(aes(exp(.epred), y = condition, fill = condition)) + 
+  stat_interval(.width = c(0.53, 0.95), alpha = 0.75, stroke = 5) +
+  scale_color_brewer() + 
+  scale_x_log10()
+
+m_blks <- brm(logrt ~ 0 + condition + (1|participant), 
+              data = lego_search,
+              prior = my_priors,
+              chains = 4)
+
+# plot fixed effects
+lego_search %>% modelr::data_grid(condition) %>% 
+  add_epred_draws(m_blks, re_formula = NA) %>%
+  select(-.chain, -.iteration) %>%
+  mutate(condition = as_factor(condition),
+         condition = fct_relevel(condition, "conjunction")) %>%
+  ggplot(aes(exp(.epred), y = condition)) + 
+  stat_histinterval(data = lego_search, aes(x = rt, fill = condition), 
+                    point_interval = NULL, breaks = 20, 
+                    alpha = 0.5,
+                    outline_bars = TRUE, slab_colour = "black",
+                    limits = c(0, 5)) + 
+  stat_interval(.width = c(0.53, 0.97)) +
+  scale_x_continuous("rt", limits = c(0, 30)) + 
+  scale_color_brewer(palette = "Purples",direction  = -1) + 
+  theme(legend.position = "none") + 
+  ggtitle("(i) Distributions & Model Predictions for Task 1A") 
+
+# compare conditions
+get_variables(m_blks)
+
+m_blks %>% spread_draws(b_conditioncolour, b_conditionconjunction, b_conditionshape) %>%
+  rename(conjunction = "b_conditionconjunction", 
+         colour = "b_conditioncolour",
+         shape = "b_conditionshape") %>%
+  mutate(d_colour = conjunction - colour,
+         d_shape = conjunction - shape) %>%
+  select(-.chain, -.iteration,- .draw) -> bs
+
+sum(bs$d_colour > 0)/nrow(bs)
+sum(bs$d_shape > 0)/nrow(bs)
+
+bs %>% select(colour, conjunction, shape) %>%
+  pivot_longer(everything(), names_to = "condition", values_to = "b") %>%
+  mutate(condition = as_factor(condition),
+         condition = fct_relevel(condition, "conjunction")) %>%
+  ggplot(aes(b, fill = condition)) + 
+  geom_density(alpha = 0.5)  + 
+  theme(legend.position = "none") +
+  ggtitle("(ii) Posterior Densities for Task 1A") 
+
 
 ########################################
 # 1B: lego dots
